75fde22ef027154d842b6e26365ef7c0b48be99f
Thiagotmdev <thiagomendes.dev@gmail.com>
2023-03-09 22:04:11 -0300

    Created a function of click for button

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,11 +2,12 @@ import * as C from './styled';
 
 type Props={
     name: string;
+    click: ()=> void;
 }
 
-export const Botao = ({name}:Props)=>{
+export const Botao = ({name, click}:Props)=>{
 
     return(
-        <C.Bt>{name}</C.Bt>
+        <C.Bt onClick={click}>{name}</C.Bt>
     );
 }
